c65b63c8882b3986c5354cad1b8d8e3845c361b6
Gregory Baltazar Avalos <sillygoose259@gmail.com>
2014-11-19 13:44:44 -0800

    new comments

--- controller/create-db.php
+++ controller/create-db.php
@@ -2,5 +2,5 @@
 require_once ("../model/database.php"); // so our program doesn't use the same information twice 
 
 
-$connection = new mysqli($host, $username, $password); // the code we are making sure isn't used twice
+$connection = new mysqli($host, $username, $password); //we are establishing the connection
 
--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            echo "This Webpage is running logically"; 
+            echo "This Webpage is running logically"; // a practice echo
         ?>
     </body>
 </html>
