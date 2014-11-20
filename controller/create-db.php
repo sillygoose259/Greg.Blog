c9c9388e83a4e962d2c5aa7b58c20050c074ae2e
Gregory Baltazar Avalos <sillygoose259@gmail.com>
2014-11-20 13:54:58 -0800

    added some new code in create-db that establishes the connection and an if and else statement that tells us if the program worked or failed. in the index we created a require_once that imported information from create-db

--- controller/create-db.php
+++ controller/create-db.php
@@ -4,3 +4,12 @@ require_once ("../model/database.php"); // so our program doesn't use the same i
 
 $connection = new mysqli($host, $username, $password); //we are establishing the connection
 
+if($connection-> connect_error) { //testing if we are connecting 
+    die("Error : " . $connection->connect_error); // the program will die if it doesn't connect
+}
+else {
+    echo "Success" . $connection->host_info; // the program will continue and give you a message saying success
+}
+
+$connection->close(); 
+
--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            echo "This Webpage is running logically"; // a practice echo
+            require_once (__DIR__ . "controller/create-db.php"); // importing the information from creat-db 
         ?>
     </body>
 </html>
