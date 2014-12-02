0834f4c9bfd98b0b7f881bb274fa5e3606d82e7f
Gregory Baltazar Avalos <sillygoose259@gmail.com>
2014-12-02 14:03:05 -0800

    in my create-db i made some lines of code more fitting when giving messages of completion or failure. I also added some new lines of code that added a new query that creates a table named posts in our database. also gave the certain items variables. in our create-db i tweaked a line of code. in my index i also tweaked a line of code. also tweaked some code in my private

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,11 +1,11 @@
 <?php
-require_once ("../model/database.php"); // so our program doesn't use the same information twice 
+require_once("/../model/database.php"); // so our program doesn't use the same information twice 
 
 
 $connection = new mysqli($host, $username, $password); //we are establishing the connection
 
 if($connection-> connect_error) { //testing if we are connecting 
-    die("Error : " . $connection->connect_error); // the program will die if it doesn't connect
+    die("<p>Error : " . $connection->connect_error . "</p>"); // the program will die if it doesn't connect
 }
 
 $exists = $connection ->select_db($database); //selecting our database if it exists or not
@@ -15,12 +15,24 @@ if(!$exists) { // an if statement that tell us if our database exists
 }
 
 if($query) {
-    echo "Succesfully created the database: " . $database; // a message that runs if our database creation is successful
+    echo "<p>Succesfully created the database:" . $database . "</p>"; // a message that runs if our database creation is successful
 }
 else {
-    echo "Database already exists"; // this else statement tells us that the database already exists
+    echo "<p>Database already exists</p>"; // this else statement tells us that the database already exists
 }
     
+    $query = $connection->query("CREATE TABLE posts (" // creating our table named post in our database
+            . "id int(11) NOT NULL AUTO_INCREMENT ("  // giving our table an id saying that it can't be empty and has to be auto incremented
+            . "title varchar(255) NOT NULL (" // the title can have up to 255 character and cannot be empty, title stored as characters
+            . "post text NOT NULL,"  // our post informaton is going to be stored as text
+            . "PRIMARY KEY (id )"); // making the primary key id
+    
+    if($query) {
+        echo "<p>Successfully created database: posts</p>"; // an echo message that tells us if our table creation was successful
+    }
+ else {
+        echo "<p>$connection->error</p>"; // a message that tells us if our table exists or not
+}
 
 $connection->close(); 
 
--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            require_once (__DIR__ . "controller/create-db.php"); // importing the information from creat-db 
+            require_once (__DIR__ . "/controller/create-db.php"); // importing the information from creat-db 
         ?>
     </body>
 </html>
