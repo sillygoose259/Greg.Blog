c3cc2e9d68f9ce669cadce4740bce18e96f94c97
Gregory Baltazar Avalos <sillygoose259@gmail.com>
2014-12-03 14:01:42 -0800

    we changed where some code was at and split it so that we could maintain it easier.

--- controller/create-db.php
+++ controller/create-db.php
@@ -11,21 +11,23 @@ if($connection-> connect_error) { //testing if we are connecting
 $exists = $connection ->select_db($database); //selecting our database if it exists or not
 
 if(!$exists) { // an if statement that tell us if our database exists
-    $query = $connection->query("CREATE DATABASE $database"); //created our database  
-}
-
-if($query) {
+    $query = $connection->query("CREATE DATABASE $database"); //created our database
+    
+    if($query) {
     echo "<p>Succesfully created the database:" . $database . "</p>"; // a message that runs if our database creation is successful
 }
 else {
     echo "<p>Database already exists</p>"; // this else statement tells us that the database already exists
 }
+}
+
+
     
     $query = $connection->query("CREATE TABLE posts (" // creating our table named post in our database
-            . "id int(11) NOT NULL AUTO_INCREMENT ("  // giving our table an id saying that it can't be empty and has to be auto incremented
-            . "title varchar(255) NOT NULL (" // the title can have up to 255 character and cannot be empty, title stored as characters
+            . "id int(11) NOT NULL AUTO_INCREMENT,"  // giving our table an id saying that it can't be empty and has to be auto incremented
+            . "title varchar(255) NOT NULL ," // the title can have up to 255 character and cannot be empty, title stored as characters
             . "post text NOT NULL,"  // our post informaton is going to be stored as text
-            . "PRIMARY KEY (id )"); // making the primary key id
+            . "PRIMARY KEY (id))"); // making the primary key id
     
     if($query) {
         echo "<p>Successfully created database: posts</p>"; // an echo message that tells us if our table creation was successful
--- index.php
+++ index.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
+
         <?php
+            require_once (__DIR__ .  "/view/header.php");
             require_once (__DIR__ . "/controller/create-db.php"); // importing the information from creat-db 
+            require_once (__DIR__ . "/view/footer.php");
+            
         ?>
-    </body>
-</html>
+    
