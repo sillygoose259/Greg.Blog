d8ff52083715fed7ff426dec3148aaf04e309d05
Gregory Baltazar Avalos <sillygoose259@gmail.com>
2014-12-12 13:56:51 -0800

    added a new require once that goes to navigation.php added lines of code that add a text box where you can enter in the name of your entry the text that is going to be in your entry and added a submit button to send your text.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 
         <?php
             require_once (__DIR__ .  "/view/header.php");
+            require_once (__DIR__ . "/view/navigation.php");
             require_once (__DIR__ . "/controller/create-db.php"); // importing the information from creat-db 
             require_once (__DIR__ . "/view/footer.php");
             
---
+++ view/form.php
@@ -0,0 +1,18 @@
+<h1>Create Blog Post</h1>
+
+<form>
+ <div>
+     <label for="title">Title :</label>
+     <input type ="text" name = "title"/>
+     <!-- LAbel the title of your entry -->
+ </div>
+     
+ <div>
+     <label for="post">Post :</label> 
+     <!-- adds a text box where you can enter in text -->
+     <textarea name="post"></textarea>
+</div>
+    <div>
+        <button type = "Submit"Submit</button> <!--a button to submit your blogs -->
+    </div>
+</form>
